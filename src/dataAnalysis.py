99c12990a36d05aa3be2f178ad9e5ce520deb395
Ethan <5279738@qq.com>
2018-01-06 15:57:52 +0800

    eda

---
+++ src/dataAnalysis.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+
+
+def load_dataset(path):
+    """
+    string  :param path: the path to load
+    dataframe  :return: dataframe of full data
+    """
+    df = pd.read_csv(path,index_col='id',encoding='gb2312')
+    return df
+
+def autolabel(ax,rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
+                ha='center', va='bottom')
+
+def create_Histgram(dataframe,imgpath='../img/'):
+    """
+    create histgram for each feather except string and datetime type
+    :param dataframe:
+    :param imgpath:the path to sava
+    :return:
+    """
+    columns = dataframe.columns.drop(['性别','体检日期'])
+    for i in columns:
+        fig = plt.figure()
+        ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+        dataframe[i].plot(kind='hist',grid=False,ax=ax1)
+        autolabel(ax=ax1,rects=ax1.patches)
+        plt.title(str(i))
+        # plt.show()
+        plt.savefig(imgpath+str(i)+"---histgram.png")
+        plt.close()
+
+def get_numerical_columns(data,tmp_path="../tmp/"):
+    """
+
+    :param data:dataframe
+    :return:
+    """
+    data_numerical = data[data.columns.drop(['性别', '体检日期'])]
+
+    df_describe = data_numerical.describe()
+    df_describe.ix['num_missing'] = len(data_numerical) - df_describe.ix['count']
+    # print(df_describe)
+    df_describe.to_csv(tmp_path+"describe.csv")
+    return data_numerical
+
+def get_corr(data,tmp_path="../tmp/"):
+    """
+
+    :param data: origal dataframe
+    :param tmp_path: temp folder to save file
+    :return:correlation
+    """
+    sex_dict = {"男":1,"女":0}
+    dftemp = data[data.columns.drop('体检日期')].replace(sex_dict)
+    data_corr = dftemp.corr()
+    data_corr_blood_glucose = dftemp.corr()['血糖']
+    data_corr_blood_glucose.to_csv(tmp_path+"data_corr_blood_glucose.csv")
+    return data_corr,data_corr_blood_glucose
+
+
+filepath = "../data/d_train_20180102.csv"
+data = load_dataset(filepath)
+datalength= len(data)
+data = data.reset_index(drop=True)
+print(("总共有 {} 条 数据").format(datalength))
+# create_Histgram(data)
+data_numerical = get_numerical_columns(data)
+data_corr,data_corr_blood_glucose = get_corr(data)
+
+# print(data_corr_blood_glucose)
